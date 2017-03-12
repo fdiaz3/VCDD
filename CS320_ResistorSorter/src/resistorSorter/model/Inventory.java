2860bbffc8d2159c41777649fcd7e251a01c8431
Darren Webb <dwebb4@ycp.edu>
2017-03-12 13:15:33 -0400

    The add and remove rack methods now take in 2 doubles instead of string

--- CS320_ResistorSorter/src/resistorSorter/model/Inventory.java
+++ CS320_ResistorSorter/src/resistorSorter/model/Inventory.java
@@ -50,23 +50,27 @@ public class Inventory {
 	
 	//Method that lets user/ admin add a rack
 	//Assume user will enter values like "5 0.25"
-	public void addRack(String s){
-		String p = "%, ";								//Prepare to append to "5" -> "5%, "
-		String w = "w";									//Prepare to append to "0.25" -> "0.25w"
-		String[] result = s.split("\\s");				//Split string s into two substrings at space
-		double tol = Double.parseDouble(result[0]);		//Convert "5" into double
-		double watt = Double.parseDouble(result[1]);	//Convert "0.25" into double
-		s = result[0]+p+result[1]+w;					//Creating string object "5 0.25" -> "5%, 0.25w"
+	public void addRack(double to, double wa){
+		String p = "%, ";								//Prepare to append to tol -> "%, "
+		String w = "w";									//Prepare to append to "watt -> "w"
+		double tol = to;								//Initialize tol to (double)to
+		double watt = wa;								//Initialize watt to (double)wa
+		String s = null;								//Initialize string s to null
+		s = tol + p + watt + w; 						//Creating string object "5%, 0.25w"
+		System.out.println(s);
 		m.put(s, new Rack(tol, watt));					//Add rack to HashMap with our key and new Rack object
 	}
 	
 	//Method that lets user remove a rack object
 	//Assume user will enter values like "5 0.25"
-	public void removeRack(String s){
+	public void removeRack(double to, double wa){
 		String p = "%, ";										//Prepare to append to "5" -> "5%, "
 		String w = "w";											//Prepare to append to "0.25" -> "0.25w"
-		String[] result = s.split("\\s");						//Split string s into two substrings at space
-		s = result[0]+p+result[1]+w;							//Creating string object "5 0.25" -> "5%, 0.25w"
+		double tol = to;										//Initialize tol to (double)to
+		double watt = wa;										//Initialize watt to (double)wa
+		String s = null;										//Initialize string s to null
+		s = tol + p + watt + w; 								//Creating string object "5%, 0.25w"
+		System.out.println(s);
 		if(m.containsKey(s)){									//Checking to see if desired rack exists
 			m.remove(s);										//Removing rack
 			System.out.println("Rack successfully removed");	//Inform user that rack was removed
