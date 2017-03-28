17d00a175eb7079a93a9d1d04088acefa388570f
Benjamin Linne <benjinne@gmail.com>
2017-03-28 08:25:50 -0400

    nothing

--- CS320_ResistorSorter/src/resistorSorterdb/persist/IDatabase.java
+++ CS320_ResistorSorter/src/resistorSorterdb/persist/IDatabase.java
@@ -7,9 +7,5 @@ import edu.ycp.cs320.booksdb.model.Book;
 import edu.ycp.cs320.booksdb.model.Pair;
 
 public interface IDatabase {
-	public List<Pair<Author, Book>> findAuthorAndBookByTitle(String title);
-	public List<Pair<Author, Book>> findAuthorAndBookByAuthorLastName(String lastname);
-	public Pair<Author, Book> insertBooks(String lastname, String firstname, String title, String isbn,
-			String published);
 	
 }
