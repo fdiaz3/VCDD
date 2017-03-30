1004f2741973fe2c403ad7348b87500a8261236f
Benjamin Linne <benjinne@gmail.com>
2017-03-29 20:36:32 -0400

    added testViewInventory

---
+++ CS320_ResistorSorter/src/resistorSorter/servlet/TestViewInventoryServlet.java
@@ -0,0 +1,49 @@
+package resistorSorter.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import resistorSorterdb.persist.DerbyDatabase;
+import resistorSorterdb.persist.DatabaseProvider;
+import resistorSorterdb.persist.IDatabase;
+import resistorSorter.model.Inventory;
+
+public class TestViewInventoryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private Inventory model;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.getRequestDispatcher("/_view/TestViewInventory.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		DatabaseProvider.setInstance(new DerbyDatabase());
+		IDatabase db = DatabaseProvider.getInstance();
+		
+		//List<Inventory> inventories = db.getAllInventories();
+		//ArrayList<Inventory> inventories = new ArrayList<Inventory>();
+		//inventories.add(new Inventory(10, 10));
+		
+		
+		//req.setAttribute("inventories", inventories);
+
+		
+		req.setAttribute("inventory", new Inventory(10, 10));
+		
+		req.getRequestDispatcher("/_view/TestViewInventory.jsp").forward(req, resp);
+	}
+	
+}
---
+++ CS320_ResistorSorter/war/_view/TestViewInventory.jsp
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+
+
+<html>
+	<head>
+		<title>Take Resistor</title>
+		<style type="text/css">
+		.error {
+			color: red;
+		}
+		
+		td.label {
+			text-align: right;
+		}
+		</style>
+	</head>
+
+	<body>
+		<c:if test="${! empty errorMessage}">
+			<div class="error">${errorMessage}</div>
+		</c:if>
+		
+		<form action="${pageContext.servletContext.contextPath}/TestViewInventory" method="post">
+
+			<table>
+			
+				<tr>
+					<td class="label">Inventory #: </td> <td class="label">BinCapacity: </td> <td class="label">RemoveLimit: </td> 
+				</tr>
+				
+				<tr>
+					<td> ${inventory.binCapacity} </td>
+				</tr>
+				
+				
+				
+				
+			
+			
+			</table>
+			
+		</form>
+		
+	</body>
+	
+</html>
