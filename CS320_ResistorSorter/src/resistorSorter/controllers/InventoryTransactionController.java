e2efdb421b1d1244317be9ec0c4caa9c9fb6cf27
Benjamin Linne <benjinne@gmail.com>
2017-04-20 11:05:16 -0400

    redo

---
+++ CS320_ResistorSorter/src/resistorSorter/controllers/InventoryTransactionController.java
@@ -0,0 +1,47 @@
+package resistorSorter.controllers;
+import java.sql.Timestamp;
+import java.util.List;
+
+import resistorSorter.model.Bin;
+import resistorSorter.model.InventoryTransaction;
+import resistorSorter.persist.DatabaseProvider;
+import resistorSorter.persist.DerbyDatabase;
+import resistorSorter.persist.IDatabase;
+import resistorSorter.persist.TestDerbyDatabase;
+
+public class InventoryTransactionController {
+
+	InventoryTransaction model;
+	IDatabase db;
+	
+	public InventoryTransactionController(InventoryTransaction model, String database){
+		if (database == "inventory"){
+			DatabaseProvider.setInstance(new DerbyDatabase());
+		}else{
+			DatabaseProvider.setInstance(new TestDerbyDatabase());
+		}
+		db = DatabaseProvider.getInstance();
+		this.model = model;
+	}
+	
+	//Empty Constructor
+	public InventoryTransactionController(String database){
+		if (database == "inventory"){
+			DatabaseProvider.setInstance(new DerbyDatabase());
+		}else{
+			DatabaseProvider.setInstance(new TestDerbyDatabase());
+		}
+		db = DatabaseProvider.getInstance();
+	}
+	
+	public void addTransaction(String userName, int bin_id, int quantity, String transactionType){
+		Timestamp transactionTime = new Timestamp(System.currentTimeMillis());
+		db.addTransaction(userName, bin_id, transactionTime, transactionType, quantity);
+	}
+	
+	public List<InventoryTransaction> displayInventoryTransactions(String username){
+		return db.getAllTransactions(username);
+	}
+
+		
+}
